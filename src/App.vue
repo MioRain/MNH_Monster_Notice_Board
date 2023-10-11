2b407a301c03ff1b580c6d68ca8c4f7c4aca001f
MioRain <ericzero1593@hotmail.com>
2023-10-11 19:48:29 +0800

    feat: add feature buttons layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <script setup>
+import MonsterCard from "./components/MonsterCard.vue";
+import FeatureButton from "./components/FeatureButton.vue";
 </script>
 
 <template>
+  <MonsterCard />
+  <FeatureButton />
 </template>
 
-<style scoped>
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+@media (min-width: 768px) {
+  #app {
+    width: 80vw;
+    height: 90vh;
+    margin: 5vh auto 5vh auto;
+    border: 3px solid #c0b08e;
+    border-radius: 30px;
+  }
+}
 </style>
---
+++ src/components/FeatureButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <button class="announce">發布</button>
+    <button class="city">城市</button>
+    <button class="monster">魔物</button>
+    <button class="search">搜尋</button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100px;
+  border-top: 5px solid #c0b08e;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  & button {
+    width: 65px;
+    height: 65px;
+    background-color: rgba(226, 240, 214, 0.45);
+    border: 1px solid #c0b08e;
+    border-radius: 50%;
+    cursor: pointer;
+
+    &:hover {
+      background-color: rgba(226, 240, 214, 0.45);
+      border: 1px solid #c0b08e;
+      opacity: 0.3;
+    }
+
+    &:active {
+      background-color: rgb(226, 240, 214);
+      border: 1px solid #83765b;
+      opacity: 1;
+    }
+  }
+}
+</style>
